ed9fdfb0f8e7d4fb03f1e94d979cd9f587667a71
AndresLunaH <androslunah@gmail.com>
2023-08-29 22:43:26 -0600

    v3

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ document.getElementById('swithToReg').onclick=switchToReg;
 document.getElementById('swithToLogin').onclick=switchToLogin;
 
 //--Hacer cambio a registrarse--//
-    function switchToReg() {
+    function switchToReg(){
         document.getElementById('signin').style = "display:inline-block";
         document.getElementById('login').style = "display:none";
     }
-    function switchToLogin() {
+    function switchToLogin(){
         document.getElementById('signin').style = "display:none";
         document.getElementById('login').style = "display:inline-block";
     }
