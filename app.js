328a0133c87209645a6038778688770f6ed6fd22
AndrosLunaH <androslunah@gmail.com>
2023-08-31 00:56:11 -0600

    mielecita dulce

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+//
 const loginForm= document.getElementById('login')
 const loginButton= document.getElementById("login-form-submit")
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+    //--Asignación de usuarios--//
+const users = [
+    {user:'mali', password:'1234', balance: 1000},
+    {user:'gera', password: '1234', balance: 1500},
+    {user:'maui', password:'1234', balance: 2500}
+];
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        alert("You have successfully logged in.");
+    
+    if (username === "u.user" && password === "u.password") {
+        alert("Bienvenido.");
         location.reload();
     } else {
-        loginErrorMsg.style.opacity = 1;
+        alert('Prueba con otra contraseña');
     }
+    
 })
