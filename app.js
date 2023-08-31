263e4fc999abfe6188023e77fcd478292cd1a4a1
AndrosLunaH <androslunah@gmail.com>
2023-08-30 23:41:56 -0600

    functional

--- app.js
+++ app.js
@@ -1,75 +1,15 @@
-//-- Validación--//
-const loginForm = document.getElementById('login');
-loginForm.addEventListener('submit', validateLoginForm);
-function validateLoginForm(event) {
-    event.preventDefault();
+const loginForm= document.getElementById('login')
+const loginButton= document.getElementById("login-form-submit")
 
-    const usuario = document.getElementById('usuario');
-    const password = document.getElementById('password');
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
 
-    // Validadar usuario
-    if (!validateUsuario(usuario.value)) {
-        alert('Agregar usuario correcto.');
-        return;
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
     }
-
-    // Validaar password
-    if (!validatePassword(password.value)) {
-        alert('Agrega un PIN valido.');
-        return;
-    }
-
-    // If all validations pass, submit the form
-    alert('Login successful!');
-}
-
-function validateUsuario(usuario) {
-    // Usar un usuario comun
-    const usuarioRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return usuarioRegex.test(usuario);
-}
-
-function validatePassword(password) {
-    // Hay que validar las reglas minimas correctas
-    const minLength = 8;
-    const hasUpperCase = /[A-Z]/.test(password);
-    const hasLowerCase = /[a-z]/.test(password);
-    const hasNumber = /\d/.test(password);
-
-    return (
-        password.length >= minLength &&
-        hasUpperCase &&
-        hasLowerCase &&
-        hasNumber
-    );
-}
-//--Asignacion de clase--//
-class Usuario{
-    constructor(id, usuario, saldo, password){
-        (this.id=id),
-        (this. usuario= usuario);
-        (this, saldo= saldo);
-        (this, password= password);
-    }
-}
-//--Asignación de usuarios--//
-let usuarios=[
-    new Usuario(
-        id=1,
-        nombre= "Mali",
-        saldo= 290,
-        contraseña= 1234
-    ),
-    new Usuario(
-        id=2,
-        nombre= "Gera",
-        saldo= 290,
-        password= 54321
-        ),
-    new Usuario(
-        id=3 ,
-        nombre="Maui",
-        saldo= 67,
-        password=7654 
-    )
-]
+})
